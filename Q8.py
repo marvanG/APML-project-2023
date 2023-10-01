0f03f69cc5648c2375b9eeda64622a3582318240
Your Name <you@example.com>
2023-10-01 18:26:15 +0200

    geia

--- Q8.py
+++ Q8.py
@@ -92,7 +92,7 @@ pt_m , pt_v = truncGaussMM (a, b, mu_6_mean , mu_6_var)
 mu_9_mean,mu_9_var=divideGauss(pt_m,pt_v,mu_6_mean,mu_6_var)
 
 
-p_mean,p_var=mutiplyGauss(s1-s2,mu_9_mean,prior_s3_var,mu_9_var)
+p_mean,p_var=mutiplyGauss(s1-s2,prior_s3_var,mu_9_mean,mu_9_var)
 mu_10_mean=(p_mean*prior_s1_var+prior_s1_mean(p_var+prior_s1_var))/(2*prior_s1_var+p_var)
 mu_10_var=(prior_s1_var*(p_var+prior_s1_var))/(2*prior_s1_var+p_var)
 mu_5_mean=(p_mean*prior_s2_var+prior_s2_mean(p_var+prior_s2_var))/(2*prior_s2_var+p_var)

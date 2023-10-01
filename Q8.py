42d4830df29f82cf0f1e0fe182e75d5a62c6383f
Steven01310131 <stefanos.tsabanakis@gmail.com>
2023-10-01 19:18:13 +0200

    finished with Q8 not good results

--- Q8.py
+++ Q8.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import numpy as np
 from scipy . stats import truncnorm
 import matplotlib.pyplot as plt
@@ -53,10 +52,10 @@ prior_s1_mean=25
 prior_s2_mean=25
 prior_s1_var=64
 prior_s2_var=64
-prior_s3_var=20
+prior_s3_var=80
 
-s1 = np.random.normal(prior_s1_mean, prior_s1_var)
-s2 = np.random.normal(prior_s2_mean, prior_s2_var)
+s1 = 25
+s2 = 25
 
 
 y=1
@@ -64,21 +63,21 @@ y=1
 #factor functions 
 factor_s1_mean=prior_s1_mean
 factor_s2_mean=prior_s2_mean
-factor_s2_var=16
+factor_s2_var=80
 
 #messages
 mu_1_mean=prior_s1_mean
-mu_1_var=prior_s1_mean
+mu_1_var=prior_s1_var
 mu_2_mean=prior_s1_mean
-mu_2_var=prior_s1_mean
+mu_2_var=prior_s1_var
 
-mu_3_mean=prior_s2_var
+mu_3_mean=prior_s2_mean
 mu_3_var=prior_s2_var
-mu_4_mean=prior_s2_var
+mu_4_mean=prior_s2_mean
 mu_4_var=prior_s2_var
 
-mu_6_mean=mu_2_mean-mu_3_mean
-mu_6_var=mu_2_var+mu_3_var+factor_s2_var
+mu_6_mean=mu_2_mean-mu_4_mean
+mu_6_var=mu_2_var+mu_4_var+factor_s2_var
 
 if y == 1:
     a, b = 0, np.Inf
@@ -87,16 +86,16 @@ else:
 
 #here we find the mean and variance of the truncated based on the result of the game
 
-pt_m , pt_v = truncGaussMM (a, b, mu_6_mean , mu_6_var)
+qt_m , qt_v = truncGaussMM (a, b, mu_6_mean , mu_6_var)
 
-mu_9_mean,mu_9_var=divideGauss(pt_m,pt_v,mu_6_mean,mu_6_var)
+mu_9_mean,mu_9_var=divideGauss(qt_m,qt_v,mu_6_mean,mu_6_var)
 
 
 p_mean,p_var=mutiplyGauss(s1-s2,prior_s3_var,mu_9_mean,mu_9_var)
-mu_10_mean=(p_mean*prior_s1_var+prior_s1_mean(p_var+prior_s1_var))/(2*prior_s1_var+p_var)
-mu_10_var=(prior_s1_var*(p_var+prior_s1_var))/(2*prior_s1_var+p_var)
-mu_5_mean=(p_mean*prior_s2_var+prior_s2_mean(p_var+prior_s2_var))/(2*prior_s2_var+p_var)
-mu_5_var=(prior_s2_var*(p_var+prior_s2_var))/(2*prior_s2_var+p_var)
+mu_10_mean=p_mean
+mu_10_var=p_var+mu_2_var
+mu_5_mean=p_mean
+mu_5_var=p_var+mu_3_var
 
 
 s1_s2_mean_col = np.array([[prior_s1_mean, prior_s2_mean]]).reshape(-1,1)
@@ -106,18 +105,19 @@ s1_gibbs,s2_gibbs=gibbs_sampling(2000,s1_s2_mean_col,s_cov_matrix,6,1)
 
 
 p_mean_s1_t,p_var_s1_t=mutiplyGauss(mu_10_mean,mu_10_var,mu_1_mean,mu_1_var)
-
-plt.hist(s1_gibbs[200:], bins=50, color='blue', alpha=0.7)
-plt.hist(s2_gibbs[200:], bins=50, color='red', alpha=0.7)
-plt.show()
-
+p_mean_s2_t,p_var_s2_t=mutiplyGauss(mu_5_mean,mu_5_var,mu_4_mean,mu_4_var)
+plt.hist(s1_gibbs[200:], bins=50, density=True, label=f's1 data')
+# plt.hist(s2_gibbs[200:], bins=50, color='red', alpha=0.7)
+# plt.show()
+print(p_mean_s1_t)
+print(np.mean(s1_gibbs))
 # mean = 0  # Mean (μ)
 # variance = 1  # Variance (σ²)
-# x = np.linspace(mean - 4 * np.sqrt(variance), mean + 4 * np.sqrt(variance), 1000)  # Adjust the range as needed
-# y = norm.pdf(x, loc=mean, scale=variance)
-# plt.plot(x, y, label='Gaussian Distribution')
-# plt.title('Gaussian Distribution')
-# plt.xlabel('X')
-# plt.ylabel('Probability Density')
-# plt.legend()
-# plt.show()
+x = np.linspace(p_mean_s1_t- 4 * np.sqrt(p_var_s1_t), p_mean_s1_t + 4 * np.sqrt(p_var_s1_t), 1000)  # Adjust the range as needed
+y = norm.pdf(x, loc=p_mean_s1_t, scale= np.sqrt(p_var_s1_t))
+plt.plot(x, y,color='r', label='Gaussian Distribution')
+plt.title('Gaussian Distribution')
+plt.xlabel('X')
+plt.ylabel('Probability Density')
+plt.legend()
+plt.show()
